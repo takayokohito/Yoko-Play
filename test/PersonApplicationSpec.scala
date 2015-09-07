5b11e5322455465aa4a04e8f20fca20e006464ef
takayokohito <yokoyama@opentone.co.jp>
2015-09-07 11:15:40 +0900

    Actionの修正 Testコードの作成

--- test/PersonApplicationSpec.scala
+++ test/PersonApplicationSpec.scala
@@ -1,10 +1,54 @@
-import org.specs2.mutable.Specification
 import play.api.mvc.Results
-import play.api.test.Helpers._
-import play.api.test.{PlaySpecification, FakeHeaders, FakeRequest, WithApplication}
-import play.libs.Json
+import play.api.test.{FakeHeaders, FakeRequest, PlaySpecification, WithApplication}
 
 
 class PersonApplicationSpec extends PlaySpecification with Results {
+
+  "PersonApplication#inputPersonInfo" should {
+    // 正常系のテスト
+    "response OK." in new WithApplication {
+      val request = """{"age":24,"name":{"firstName":"山田","lastName":"太郎"}}"""
+      val Some(result) = route(
+        FakeRequest(
+          "POST",
+          "/sample/person",
+          FakeHeaders(Seq("Content-type" -> "application/json")),
+          request
+        )
+      )
+      status(result) mustEqual OK
+      contentAsString(result) mustEqual (request)
+    }
+
+    // 異常系のテスト(単数)
+    "response NG when request bad 1 param." in new WithApplication {
+      val request = """{"age":24,"name":{"firstNamexx":"山田","lastName":"太郎"}}"""
+      val Some(result) = route(
+        FakeRequest(
+          "POST",
+          "/sample/person",
+          FakeHeaders(Seq("Content-type" -> "application/json")),
+          request
+        )
+      )
+      status(result) mustEqual BAD_REQUEST
+      contentAsString(result) mustEqual ("""{"obj.name.firstName":[{"msg":["error.path.missing"],"args":[]}]}""")
+    }
+
+    // 異常系のテスト(複数)
+    "response NG when request bad 2 params." in new WithApplication {
+      val request = """{"agexx":24,"name":{"firstNamexx":"山田","lastName":"太郎"}}"""
+      val Some(result) = route(
+        FakeRequest(
+          "POST",
+          "/sample/person",
+          FakeHeaders(Seq("Content-type" -> "application/json")),
+          request
+        )
+      )
+      status(result) mustEqual BAD_REQUEST
+      contentAsString(result) mustEqual ("""{"obj.age":[{"msg":["error.path.missing"],"args":[]}],"obj.name.firstName":[{"msg":["error.path.missing"],"args":[]}]}""")
+    }
+  }
 
 }
